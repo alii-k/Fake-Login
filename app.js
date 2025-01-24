883a7fd872bf0de0419f24dabadca2cbd6822700
Ali Kamal <127852541+alii-k@users.noreply.github.com>
2025-01-24 05:01:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 import express from 'express';
 import { createClient } from '@supabase/supabase-js';
 
+import postgres from 'postgres'
+
+const connectionString = process.env.DATABASE_URL
+const sql = postgres(connectionString)
+
+export default sql
+
 const SUPABASE_URL = 'https://ksiadqcrthcnpxmnhxxx.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzaWFkcWNydGhjbnB4bW5oeHh4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc2ODU0NjIsImV4cCI6MjA1MzI2MTQ2Mn0.2fW5eEFWNOB-krwbnCOV6D3cY-oucCQaLG1cNrqeTE0';
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
